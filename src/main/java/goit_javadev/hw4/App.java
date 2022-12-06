e45a4b5d747905ba2dfee1f0d1faeab4d09efa17
Andy <pauchokbk@gmail.com>
2022-12-06 14:14:23 +0200

    Update Maven packages and fix TimeZone error

--- src/main/java/goit_javadev/hw4/App.java
+++ src/main/java/goit_javadev/hw4/App.java
@@ -1,16 +1,17 @@
 package goit_javadev.hw4;
 
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import goit_javadev.hw4.cli.Console;
 import goit_javadev.hw4.connection.DbConnectionProvider;
-import goit_javadev.hw4.dao.DeveloperDaoService;
-import goit_javadev.hw4.entity.Developer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.TimeZone;
 
 public class App {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kiev"));
+
         try (DbConnectionProvider connectionProvider = new DbConnectionProvider()) {
             Connection connection = connectionProvider.createConnection();
 
